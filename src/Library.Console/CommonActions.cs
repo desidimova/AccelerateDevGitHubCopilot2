319f841c271630504f30bfe6a96ab5c016b3278f
Desi Dimova <desislava.dimova@createfuture.com>
2025-11-25 16:20:04 +0200

    Implement book search functionality and update CommonActions enum

--- src/Library.Console/CommonActions.cs
+++ src/Library.Console/CommonActions.cs
@@ -1,5 +1,3 @@
-namespace Library.Console;
-
 [Flags]
 public enum CommonActions
 {
@@ -9,5 +7,6 @@ public enum CommonActions
     SearchPatrons = 4,
     RenewPatronMembership = 8,
     ReturnLoanedBook = 16,
-    ExtendLoanedBook = 32
+    ExtendLoanedBook = 32,
+    SearchBooks = 64
 }
